824c230944f8c093ad973033a06a4fa787672f6e
ljc1928 <ljc1928@naver.com>
2021-04-27 23:56:36 +0900

    Unit_7 2nd Test

---
+++ CodingDojang/Unit_7_2nd.c
@@ -0,0 +1,15 @@
+/*다음 소스 코드를 완성하여 11이 출력되게 만드세요..
+
+정답에는 밑줄 친 부분에 들어갈 코드만 작성해야 합니다.*/
+
+#include <stdio.h>
+ 
+int main()
+{
+    /* 정답 */char num1;
+    /* 부분 */short num2;
+ 
+    printf("%d\n", sizeof(num1) + sizeof(num2) + sizeof(long long));
+ 
+    return 0;
+}
